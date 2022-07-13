3f654e8c0985a0dd03834260a1c6a24f10b71f04
removedporn <86824510+removedporn@users.noreply.github.com>
2022-07-13 15:01:36 +0800

    Delete tests directory

--- tests/data/src/pep518_with_namespace_package-1.0/setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup
-
-import simple_namespace.module
-
-setup(
-    name='pep518_with_namespace_package',
-    version='1.0',
-    py_modules=['pep518_with_namespace_package'],
-)
